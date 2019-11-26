c224fb0dd7ee35488a5f3d28f66f4a9f00033bdd
Stefan Milosavljevic <stefanmfkg@live.com>
2019-11-26 08:41:11 +0100

    promena naziva

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{csrf_token()}}">
-    <title>Rent-A-Car Hit Auto</title>
+    <title>Hit Auto</title>
     <link href="{{URL::asset('css/reset.css')}}" rel="stylesheet">
     <link href="{{URL::asset('/css/stilovi.css')}}" rel="stylesheet">
     <link href="{{URL::asset('/css/bootstrap.css')}}" rel="stylesheet">
