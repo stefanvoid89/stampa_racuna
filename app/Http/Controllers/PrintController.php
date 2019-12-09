5f361490b7eb84ef4b6dbe0595fa1d25cb2a3fc3
Stefan Milosavljevic <stefanmfkg@live.com>
2019-12-09 16:04:17 +0100

    hotfix -- storno racun i dodvanje abono 2

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -133,71 +133,9 @@ class PrintController extends Controller
 
     public function print($id)
     {
+        $header = collect(DB::select("EXEC _MiServiceHeader :id", ['id' => $id]))->first();
 
-
-        $header = collect(DB::select("SELECT   top 1  nal.NumOT as BrojNaloga, nal.AnoOT as GodinaNaloga
-        ,case when nal.taller=2 then 'motrio'
-        else case when nal.marca='REN' then 'renault' when nal.marca='DAC' then 'dacia' when nal.marca='NIS' then 'nissan' else 'renault' end end as Marca
-        , case when nal.taller=2 then 'vidikovac' else 'sajmiste' end  as Lokacija,
-        ltrim(rtrim(convert(char,nal.FechaAperturaOT,103))) + ' ' + right('0'+ltrim(rtrim(cast(nal.EntregaRealHora as int))),2)
-        + ':'+ right('0'+ltrim(rtrim(cast(nal.EntregaRealMinuto as int))),2) as DatumPrijema
-        ,convert(char,isnull(nal.EntregaPrevFecha,fak.fechaFactura ),103) as PredvidjeniDatumIzdavanja,
-        nal.ClienteFac as NalogoDavac,  nal.Recepcionista as Korisnik,
-        (select top 1 nombre from [ttRecepcionista] r where nal.Recepcionista= r.Recepcionista and nal.taller=r.taller) as RecepcionisatName,
-        ltrim(rtrim(isnull(nal.Nombre,'')+' '+isnull(nal.apellido1,''))) as ImeKupca, nal.direccion as Adresa, nal.CPostal as Posta, isnull(nal.cif,'') as Pib,
-        nal.perJuridica as JelFirma, k.NIntraStat as Maticni,
-        ltrim(rtrim(isnull(k.Nombre,'')+' '+isnull(k.apellido1,''))) as ImeKupcaSif, k.direccion as AdresaSif, k.CPostal as PostaSif, isnull(k.cif,'') as PibSif,
-        k.perJuridica as JelFirmaSif, k.NIntraStat as MaticniSif,p.Descrip as MestoSif,
-        case when fak.Abono = 1 then 'STORNO RAČUN' else 'RAČUN ' end as OPisDokumena, fak.anoFactura as GodinaRačuna, ltrim(rtrim(cast(fak.anoFactura as char))) + '/' + ltrim(rtrim(cast(fak.Factura as char)))  as BrojRacuna, convert(char,fak.fechaFactura,103) as DatumRacuna,
-        kup.TallModoPago as NacinPalcanjaSifra,
-        (select descrip  from tgModoPago where codigo=kup.TallModoPago) as NacinPlacanjaNaziv , kup.Cliente as KupacNaFakturi,
-        (select numtel1 from tgcliente where codigo=kup.Cliente) as Telefon, convert(char,nal.FecMatricPrimera,103) as datumPrveReg,nal.km as Kilom, isnull(nal.numPrisma,'') as BrojPrijema,
-        nal.Siniestro As brojOdobrenja, (select descrip from tgVersion  where Modelo =nal.modelo and marca =nal.marca and version=nal.version) As MarkaModel,
-        nal.Matric as RegBroj, nal.Chasis as Sasija, nal.codigofabricacion as ProizvodniBroj, convert(char,fak.FechaFactura,103) as Datumprometa, '' as BIR
-        from  ttotCab nal
-        inner join ttotfac fak on nal.NumInterno=fak.numinterno
-        inner join ttotCargo kup on kup.Numinterno=nal.NumInterno and kup.cargo=fak.NumIntCargo
-        inner join tgcliente k on k.codigo =kup.Cliente
-        left  join tgpobla p on id=k.pobla
-        where 1=1 and fak.NumIntFac = :id", ['id' => $id]))->first();
-
-
-
-        $positions =  collect(DB::select("SELECT * from (
-            select distinct  i.intervencion ,0 as RBR, i.Referencia as Sifra,cast(i.Descrip as varchar(max)) as Opis , str(0.00,8,2) as Kolicina,
-            str(i.TotalNetoIntervencion,8,2) as Cena,str(0.00,8,2) as Popust,str(0.00,8,2) as NetoCena ,
-            str(0.00,8,2) as impneto,str(0.00,8,2) as UkupnoRadBruto,str(0.00,8,2) as ukupnoDeoBruto,str(0.00,8,2) as UkupnoOstaloBruto, str(0.00 ,8,2) as PopustUsluge,
-            str(0.00,8,2) as PopustDeo,str(0.00,8,2) as PopustOstalo,str(0.00,8,2) as OsnovicaZAPdv,  str(0.00,8,2) as  UkupnoPDV, str(0.00,8,2)  as UkupnoRacun,
-            str(0.00,8,2) as RadNeto,str(0.00,8,2) as DeoNeto,str(0.00,8,2)as NetoOstalo
-            from ttotCab nal
-            inner join ttotfac fak on nal.NumInterno=fak.numinterno
-            inner join ttotCargo kup on kup.Numinterno=nal.NumInterno and kup.cargo=fak.NumIntCargo
-            inner join ttOtIntervencion I on i.Numinterno = kup.Numinterno
-            inner join ttOTCargoInt ki on ki.NumIntOT=nal.NumInterno and ki.NumIntCargo=fak.NumIntCargo and ki.NumIntIntervencion=i.Intervencion
-           -- cross apply sys.objects o
-            where 1=1
-            --and  object_id	 < 30
-            and fak.NumIntFac  = :id
-            union all
-            SELECT	i.Intervencion,row_number() over(order by i.intervencion, l.referencia) as RBR, l.Referencia As Sifra,l.Descrip as Opis, format(l.CantidadHoras * (1-fak.abono * 2),'N2') as Kolicina,
-            format(l.PrecioUnitario,'N2') as Cena,format(l.Descuento,'N2')  as Popust, format(l.PrecioUnitario *(1-l.Descuento/100) ,'N2') as NetoCena ,
-            format(l.impneto * (1-fak.abono * 2),'N2') ,format(kup.ImpBrutoMO * (1-fak.abono * 2),'N2')  as UkupnoRadBruto,format(kup.ImpBrutoRec * (1-fak.abono * 2),'N2')  as ukupnoDeoBruto,
-            format(kup.ImpBrutoTraSub * (1-fak.abono * 2),'N2')  as UkupnoOstaloBruto, format(kup.ImpDtoMO * (1-fak.abono * 2),'N2') as PopustUsluge,
-            format(kup.ImpDtoRec * (1-fak.abono * 2),'N2')  as PopustDeo,format(kup.ImpDtoTraSub * (1-fak.abono * 2),'N2')  as PopustOstalo,format((kup.ImpFactura - kup.impiva) * (1-fak.abono * 2),'N2')  as OsnovicaZAPdv,
-            format(kup.impiva * (1-fak.abono * 2),'N2')  as UkupnoPDV, format(kup.ImpFactura * (1-fak.abono * 2),'N2')  as UkupnoRacun,
-            format((kup.ImpBrutoMO-kup.ImpDtoMO) * (1-fak.abono * 2),'N2') as RadNeto,format((kup.ImpBrutoRec-kup.ImpDtoRec)* (1-fak.abono * 2),'N2') as DeoNeto,format((kup.ImpBrutoTraSub-kup.ImpDtoTraSub) * (1-fak.abono * 2),'N2') as NetoOstalo
-            from ttotCab nal
-            inner join ttotfac fak on nal.NumInterno=fak.numinterno
-            inner join ttotCargo kup on kup.Numinterno=nal.NumInterno and kup.cargo=fak.NumIntCargo
-            inner join ttOTCargoInt ki on ki.NumIntOT=nal.NumInterno and ki.NumIntCargo=fak.NumIntCargo
-            inner join ttOTIntervencion i on i.intervencion=ki.NumIntIntervencion
-            inner join ttOTLinea L on l.NumIntOT=ki.NumIntOT and l.NumIntIntervencion=ki.NumIntIntervencion
-            -- cross apply sys.objects o
-            where 1=1
-            --  and  object_id	 < 30
-            and l.CantidadHoras<>0
-            and fak.NumIntFac = :id2
-            )q order by Intervencion, rbr", ['id' => $id, 'id2' => $id]));
+        $positions =  collect(DB::select("EXEC _MiServicePositions :id", ['id' => $id]));
 
         $positions_sum = $positions->firstWhere('RBR', '1');
 
